0342f3ea2ed8dda6f531d2688050f33a5a2d6dd0
Ruslan Yarmak <ruslan.yarmak.intech@gmail.com>
2024-08-19 17:08:28 +0300

    Updated 0.17 NEW visual

--- Ruslan/main_dialog.py
+++ Ruslan/main_dialog.py
@@ -20,43 +20,43 @@ class MainDialog(QtWidgets.QDialog):
         self.table_names = ['Transactions_history', 'Customers', 'Stock', 'Products']
 
         self.setObjectName("Dialog")
-        self.setFixedSize(765, 540)
+        self.setFixedSize(1200, 940)
 
         self.tabWidget = QtWidgets.QTabWidget(self)
-        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 761, 541))
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 1200, 940))
         self.tabWidget.setObjectName("tabWidget")
 
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
 
         self.tableWidget = QtWidgets.QTableWidget(self.tab)
-        self.tableWidget.setGeometry(QtCore.QRect(10, 50, 741, 461))
+        self.tableWidget.setGeometry(QtCore.QRect(10, 50, 1175, 850))
         self.tableWidget.setObjectName("tableWidget")
         self.table_widgets.append(self.tableWidget)
         self.table_widgets[0].setEditTriggers(QTableWidget.NoEditTriggers)
 
         self.new_transaction = QtWidgets.QPushButton(self.tab)
-        self.new_transaction.setGeometry(QtCore.QRect(640, 10, 101, 31))
+        self.new_transaction.setGeometry(QtCore.QRect(1040, 10, 131, 31))
         self.new_transaction.setObjectName("new_transaction")
 
         self.upload_document = QtWidgets.QPushButton(self.tab)
-        self.upload_document.setGeometry(QtCore.QRect(520, 10, 101, 31))
+        self.upload_document.setGeometry(QtCore.QRect(900, 10, 131, 31))
         self.upload_document.setObjectName("upload_document")
 
         self.add_document = QtWidgets.QPushButton(self.tab)
-        self.add_document.setGeometry(QtCore.QRect(280, 10, 101, 31))
+        self.add_document.setGeometry(QtCore.QRect(620, 10, 131, 31))
         self.add_document.setObjectName("add_document")
         if user != 'super':
             self.add_document.setEnabled(False)
 
         self.add_manager = QtWidgets.QPushButton(self.tab)
-        self.add_manager.setGeometry(QtCore.QRect(400, 10, 101, 31))
+        self.add_manager.setGeometry(QtCore.QRect(760, 10, 131, 31))
         self.add_manager.setObjectName("add_manager")
         if user != 'super':
             self.add_manager.setEnabled(False)
 
         self.lineEdit = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit.setGeometry(QtCore.QRect(150, 20, 113, 21))
+        self.lineEdit.setGeometry(QtCore.QRect(180, 20, 153, 21))
         self.lineEdit.setObjectName("lineEdit")
 
         self.tabWidget.addTab(self.tab, "")
@@ -65,17 +65,17 @@ class MainDialog(QtWidgets.QDialog):
         self.tab_2.setObjectName("tab_2")
 
         self.tableWidget_2 = QtWidgets.QTableWidget(self.tab_2)
-        self.tableWidget_2.setGeometry(QtCore.QRect(10, 50, 741, 461))
+        self.tableWidget_2.setGeometry(QtCore.QRect(10, 50, 1175, 850))
         self.tableWidget_2.setObjectName("tableWidget_2")
         self.table_widgets.append(self.tableWidget_2)
         self.table_widgets[1].setEditTriggers(QTableWidget.NoEditTriggers)
 
         self.lineEdit_2 = QtWidgets.QLineEdit(self.tab_2)
-        self.lineEdit_2.setGeometry(QtCore.QRect(150, 20, 113, 21))
+        self.lineEdit_2.setGeometry(QtCore.QRect(180, 20, 153, 21))
         self.lineEdit_2.setObjectName("lineEdit_2")
 
         self.add_client = QtWidgets.QPushButton(self.tab_2)
-        self.add_client.setGeometry(QtCore.QRect(640, 10, 101, 31))
+        self.add_client.setGeometry(QtCore.QRect(1040, 10, 131, 31))
         self.add_client.setObjectName("add_client")
 
         self.tabWidget.addTab(self.tab_2, "")
@@ -84,25 +84,25 @@ class MainDialog(QtWidgets.QDialog):
         self.tab_3.setObjectName("tab_3")
 
         self.tableWidget_3 = QtWidgets.QTableWidget(self.tab_3)
-        self.tableWidget_3.setGeometry(QtCore.QRect(10, 50, 741, 461))
+        self.tableWidget_3.setGeometry(QtCore.QRect(10, 50, 1175, 850))
         self.tableWidget_3.setObjectName("tableWidget_3")
         self.table_widgets.append(self.tableWidget_3)
         self.table_widgets[2].setEditTriggers(QTableWidget.NoEditTriggers)
 
         self.lineEdit_3 = QtWidgets.QLineEdit(self.tab_3)
-        self.lineEdit_3.setGeometry(QtCore.QRect(150, 20, 113, 21))
+        self.lineEdit_3.setGeometry(QtCore.QRect(180, 20, 153, 21))
         self.lineEdit_3.setObjectName("lineEdit_3")
 
         self.add_warehouse = QtWidgets.QPushButton(self.tab_3)
-        self.add_warehouse.setGeometry(QtCore.QRect(640, 10, 101, 31))
+        self.add_warehouse.setGeometry(QtCore.QRect(1040, 10, 131, 31))
         self.add_warehouse.setObjectName("add_warehouse")
 
         self.edit_warehouse = QtWidgets.QPushButton(self.tab_3)
-        self.edit_warehouse.setGeometry(QtCore.QRect(520, 10, 101, 31))
+        self.edit_warehouse.setGeometry(QtCore.QRect(900, 10, 131, 31))
         self.edit_warehouse.setObjectName("edit_warehouse")
 
         self.comboBox = QtWidgets.QComboBox(self.tab_3)
-        self.comboBox.setGeometry(QtCore.QRect(10, 20, 131, 22))
+        self.comboBox.setGeometry(QtCore.QRect(10, 20, 161, 21))
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItems(['Stock'])
         self.warehouse_count = self.add_warehouses_to_combobox()
@@ -113,17 +113,17 @@ class MainDialog(QtWidgets.QDialog):
         self.tab_4.setObjectName("tab_4")
 
         self.tableWidget_4 = QtWidgets.QTableWidget(self.tab_4)
-        self.tableWidget_4.setGeometry(QtCore.QRect(10, 50, 741, 461))
+        self.tableWidget_4.setGeometry(QtCore.QRect(10, 50, 1175, 850))
         self.tableWidget_4.setObjectName("tableWidget_4")
         self.table_widgets.append(self.tableWidget_4)
         self.table_widgets[3].setEditTriggers(QTableWidget.NoEditTriggers)
 
         self.lineEdit_4 = QtWidgets.QLineEdit(self.tab_4)
-        self.lineEdit_4.setGeometry(QtCore.QRect(150, 20, 113, 21))
+        self.lineEdit_4.setGeometry(QtCore.QRect(180, 20, 153, 21))
         self.lineEdit_4.setObjectName("lineEdit_4")
 
         self.add_product = QtWidgets.QPushButton(self.tab_4)
-        self.add_product.setGeometry(QtCore.QRect(640, 10, 101, 31))
+        self.add_product.setGeometry(QtCore.QRect(1040, 10, 131, 31))
         self.add_product.setObjectName("add_product")
 
         self.tabWidget.addTab(self.tab_4, "")
@@ -195,7 +195,7 @@ class MainDialog(QtWidgets.QDialog):
         return values
 
     def open_client_card(self, arg):
-        if arg:
+        if arg is not False:
             arg = self.get_string_values(arg)
         client_card_window = Ui_Client_Add(arg)
         resp = client_card_window.exec_()
@@ -213,7 +213,7 @@ class MainDialog(QtWidgets.QDialog):
         resp = warehouse_card_window.exec_()
 
     def open_product_card(self, arg):
-        if arg:
+        if arg is not False:
             arg = self.get_string_values(arg)
         product_card_window = AddProductDialog(arg, self.manager_id)
         resp = product_card_window.exec_()
